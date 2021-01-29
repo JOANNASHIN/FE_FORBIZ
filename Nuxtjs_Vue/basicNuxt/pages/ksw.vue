a953eb6852917d45cca43747c6a62d5b6e63a01a
kyh196201 <kyh196201@gmail.com>
2021-01-29 10:11:49 +0900

    seungwoo first commit

---
+++ Nuxtjs_Vue/basicNuxt/pages/ksw.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="test">421
+    <p class="text">5251251</p>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scope>
+  .test {
+    font: {
+      size: 40px;
+    }
+    .text {
+      font: {
+        size: 20px;
+      }
+    }
+  }
+</style>
